aa0e5198c15851fa6ce44c2e4470e0c99924227c
ankou-k <73667924+ankou-k@users.noreply.github.com>
2024-01-24 16:27:50 -0500

    binary_ne_grid

--- cagey_csp.py
+++ cagey_csp.py
@@ -84,17 +84,97 @@ An example of a 3x3 puzzle would be defined as:
 '''
 
 from cspbase import *
+import itertools
+
+def create_vars(grid_size):
+
+    # create domain
+    domain = [0]*grid_size
+    for i in range(grid_size):
+        domain[i] = i+1
+    print(domain)
+
+    # create variables
+    var_array = [0]*(grid_size*grid_size)
+
+    for i in range(grid_size):
+        for j in range(grid_size):
+            var_name = f"{i+1},{j+1}"
+            new_var = Variable(var_name, [1, 2, 3])
+            var_array[i*grid_size+j] = new_var
+
+    # return variables
+    return var_array
 
 def binary_ne_grid(cagey_grid):
-    ##IMPLEMENT
-    csp = CSP("binary_ne_grid")
+    # get grid size
+    grid_size = cagey_grid[0]
+
+    # create the variables
+    var_array = create_vars(grid_size)
+
+    # create variable domains for satisfying tuples
+    #potential issue - relative assignment
+    varDoms = [0]*2
+    domain = [0]*grid_size
+    for i in range(grid_size):
+        domain[i] = i+1
+    varDoms[0] = domain
+    varDoms[1] = domain
+
+    # create list of satisfying tuples
+    sat_tuples = []
+    for t in itertools.product(*varDoms):
+        if t[0] != t[1]:
+            sat_tuples.append(t)
+    print(sat_tuples)
+
+    # create constraints that no 2 items in a row are equal to each other
+    # and no 2 itens in a column are equal to each other
+    cons = []
+    for i in range(grid_size):
+        for comb in itertools.combinations(range(grid_size), 2):
+            # create name and scope of row constraint
+            row_name = f"Row{i+1}-{comb[0]+1},{comb[1]+1}"
+            row_scope = [var_array[i*grid_size + comb[0]], var_array[i*grid_size + comb[1]]]
+            
+            # create name and scope of column constraint
+            col_name = f"Col{i+1}-{comb[0]+1},{comb[1]+1}"
+            col_scope = [var_array[i + comb[0]*grid_size], var_array[i + comb[1]*grid_size]]
+
+            # create row and column constraints and add them to list of contraints
+            row_con = Constraint(row_name, row_scope)
+            row_con.add_satisfying_tuples(sat_tuples)
+            col_con = Constraint(col_name, col_scope)
+            col_con.add_satisfying_tuples(sat_tuples)
+            cons.append(row_con)
+            cons.append(col_con)
     
+    # create CSP
+    binary_csp = CSP("binary_ne_grid", var_array)
+    for c in cons:
+        binary_csp.add_constraint(c)
+    
+    # return csp and variables
+    return binary_csp, var_array
 
 
 def nary_ad_grid(cagey_grid):
-    ## IMPLEMENT
+
+    # get grid size
+    grid_size = cagey_grid[0]
+
+    # create the variables
+    var_array = create_vars(grid_size)
+
+    # create constraints
+
+    ## all items are different
     pass
 
 def cagey_csp_model(cagey_grid):
     ##IMPLEMENT
     pass
+
+create_vars(6)
+binary_ne_grid((3, [(3,[(1,1), (2,1)],"+"),(1, [(1,2)], '?'), (8, [(1,3), (2,3), (2,2)], "+"), (3, [(3,1)], '?'), (3, [(3,2), (3,3)], "+")]))
